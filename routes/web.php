1dbb6581d266684c9853ce97a486dd8b6a82d238
Aleksandar Milijanović <59648361+milijan00@users.noreply.github.com>
2022-11-27 11:42:36 +0100

    Deleted comments

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 Route::controller(ArticleController::class)->group(function(){
         Route::get("/articles/{role}", "index");
         Route::post("/articles/{role}", "store");
         Route::patch("/articles/{role}/{id}", "update");
         Route::patch("/articles/{role}", "buyArticle");
         Route::delete("/articles/{role}/{id}", "delete");
-//        Route::get("/articles/{role}", "index");
-//        Route::post("/articles/{role}", "store");
-//        Route::patch("/articles/{role}/{id}", "update");
-//        Route::patch("/articles/{role}", "buyArticle");
-//        Route::delete("/articles/{role}/{id}", "delete");
+
 });
 
-//Route::get('/profile', function () {
-//    //
-//})->middleware(EnsureTokenIsValid::class);
+
